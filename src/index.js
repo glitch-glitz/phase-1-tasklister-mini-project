34d67686c8a3794f7c2a5bf11a69c5357c67fbec
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-03-16 01:54:18 +0300

    Created a tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+//accessing the create-task-form & task-List
+
+const createTaskForm = document.getElementById("create-task-form");
+const taskList = document.getElementById("tasks");
+//attach an event listener after access the create-task-form
+
+createTaskForm.addEventListener("submit", (event) => {
+  event.preventDefault(); //this will prevent the page from reloading
+
+  // get the input value
+  const description = document.getElementById("new-task-description").value;
+  //make a list
+  const myList = document.createElement("li");
+  myList.textContent = description;
+  //append the task to the list
+  taskList.appendChild(myList);
+
+  // auto-clear
+  createTaskForm.reset();
 });
